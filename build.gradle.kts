30f43e6b1b1bab99845d956f597dfab2cdbad118
QuickScythe <QuickScythe@gmail.com>
2024-10-31 15:17:33 -0500

    Remove java modification

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,44 +56,6 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.register("modifyJavaField") {
-    doLast {
-        val classFile = file("build/classes/java/main/me/quickscythe/vanillaFlux/Bot.class")
-        val classReader = ClassReader(Files.newInputStream(classFile.toPath()))
-        val classWriter = ClassWriter(ClassWriter.COMPUTE_FRAMES)
-
-        classReader.accept(object : ClassVisitor(Opcodes.ASM9, classWriter) {
-            override fun visitMethod(
-                access: Int,
-                name: String,
-                descriptor: String,
-                signature: String?,
-                exceptions: Array<String>?
-            ): MethodVisitor {
-                val mv = super.visitMethod(access, name, descriptor, signature, exceptions)
-                if (name == "onInitialize") { // Target the specific method
-                    return object : MethodVisitor(Opcodes.ASM9, mv) {
-                        override fun visitCode() {
-                            super.visitCode()
-                            // Inject code at the beginning of the method
-                            mv.visitVarInsn(Opcodes.ALOAD, 0)
-                            mv.visitLdcInsn(false)
-                            mv.visitFieldInsn(Opcodes.PUTFIELD, "me/quickscythe/vanillaflux/Initializer", "DEBUG", "Ljava/lang/Boolean;")
-                        }
-                    }
-                }
-                return mv
-            }
-        }, 0)
-
-        Files.write(classFile.toPath(), classWriter.toByteArray())
-    }
-}
-
-tasks.named("jar") {
-    dependsOn("modifyJavaField")
-}
-
 publishing {
     publications {
         create<MavenPublication>("maven") {
